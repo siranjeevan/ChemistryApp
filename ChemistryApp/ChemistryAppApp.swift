ff5b0b2d479ff1417cff72eac91987aa0dcb02be
siranjeevan <smartjeevith2k@gmail.com>
2025-06-06 20:00:56 +0530

    Initial Commit

---
+++ ChemistryApp/ChemistryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemistryAppApp.swift
+//  ChemistryApp
+//
+//  Created by Jeevith  on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChemistryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
